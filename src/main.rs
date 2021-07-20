c7c2f9e2c09484bb4ae2db84125dace33a4eff15
Justis <justis710@gmail.com>
2021-07-20 09:36:33 -0400

    refactor - more generic code, dropped wrapper types, reduce footprint of tree build

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,101 @@
-type ChildNode<T> = Option<Box<BTNode<T>>>;
-
-struct BTNode<T> {
-    left: ChildNode<T>,
-    right: ChildNode<T>,
-    op: Op<T>,
+pub enum BTNode<T> {
+    Leaf(T),
+    Branch {
+        left: Box<BTNode<T>>,
+        right: Box<BTNode<T>>,
+        op: Box<dyn Fn(T, T) -> T>,
+    },
 }
 
-enum Op<T> {
-    Add,
-    Sub,
-    Div,
-    Mul,
-    Id(T)
-}
-
-impl BTNode<i32> {
-    pub fn new(op: Op<i32>, l: BTNode<i32>, r: BTNode<i32>) -> Self {
-        BTNode::<i32> {
-            op: op, left: Some(Box::new(l)), right: Some(Box::new(r))
+impl<T> BTNode<T>
+where T: std::ops::Add<Output = T>,
+{
+    pub fn add<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<BTNode<T>>,
+        R: Into<BTNode<T>>,
+    {
+        BTNode::Branch {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| l + r),
         }
     }
 }
 
-fn add_node(l: BTNode<i32>, r: BTNode<i32>) -> BTNode<i32> {
-    BTNode::new(Op::Add, l, r)
-}
-
-fn sub_node(l: BTNode<i32>, r: BTNode<i32>) -> BTNode<i32> {
-    BTNode::new(Op::Sub, l, r)
-}
-
-fn div_node(l: BTNode<i32>, r: BTNode<i32>) -> BTNode<i32> {
-    BTNode::new(Op::Div, l, r)
-}
-
-fn mul_node(l: BTNode<i32>, r: BTNode<i32>) -> BTNode<i32> {
-    BTNode::new(Op::Mul, l, r)
-}
-
-fn id_node(value: i32) -> BTNode<i32> {
-    BTNode {
-        op: Op::Id(value),
-        left: None,
-        right: None,
+impl<T> BTNode<T>
+where T: std::ops::Sub<Output = T>,
+{
+    pub fn sub<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<BTNode<T>>,
+        R: Into<BTNode<T>>,
+    {
+        BTNode::Branch {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| l - r),
+        }
     }
 }
 
-struct BinaryTree<T> {
-    head: Option<BTNode<T>>
+impl<T> BTNode<T>
+where T: std::ops::Mul<Output = T>,
+{
+    pub fn mul<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<BTNode<T>>,
+        R: Into<BTNode<T>>,
+    {
+        BTNode::Branch {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| l * r),
+        }
+    }
 }
 
-impl BinaryTree<i32> {
-    pub fn new(head: BTNode<i32>) -> Self {
-        BinaryTree::<i32> { head:  Some(head) }
+impl<T> BTNode<T>
+where T: std::ops::Div<Output = T>,
+{
+    pub fn div<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<BTNode<T>>,
+        R: Into<BTNode<T>>,
+    {
+        BTNode::Branch {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| l / r),
+        }
+    }
+}
+
+impl<T> BTNode<T> {
+    pub fn new(t: T) -> Self {
+        BTNode::Leaf(t)
     }
 
-    pub fn collapse(node: &Box<BTNode<i32>>) -> i32 {
-        let mut r: Option<i32> = None;
-        let mut l: Option<i32> = None;
-
-        if let Some(left) = &node.left {
-            l = Some(BinaryTree::collapse(left));
+    pub fn value(self) -> T {
+        match self {
+            Self::Leaf(t) => t,
+            Self::Branch { left, right, op} => op(left.value(), right.value()),
         }
+    }
+}
 
-        if let Some(right) = &node.right {
-            r = Some(BinaryTree::collapse(right));
-        }
-
-        let r = if let Some(x) = r { x } else { 0 };
-        let l = if let Some(x) = l { x } else { 0 };
-
-        match node.op {
-            Op::Add => { l + r }
-            Op::Sub => { l - r }
-            Op::Mul => { l * r }
-            Op::Div => {
-                if r == 0 {
-                    panic!("attempted divide-by-zero operation")
-                }
-                l / r
-            }
-            Op::Id(x) => x
-        }
+impl<T> From<T> for BTNode<T> {
+    fn from(t: T) -> BTNode<T> {
+        BTNode::Leaf(t)
     }
 }
 
 fn main() {
     // (10 - (2 * 2)) + (8 + (10 / 2))
-    let bt = BinaryTree::new(
-        add_node(
-            sub_node(
-                id_node(10),
-                mul_node(
-                    id_node(2),
-                    id_node(2)
-                )
-            ),
-            add_node(
-                id_node(8),
-                div_node(
-                    id_node(10),
-                    id_node(2)
-                )
-            )
-        )
+    let bt: BTNode<i32> = BTNode::add(
+        BTNode::sub(10, BTNode::mul(2, 2)),
+        BTNode::add(8, BTNode::div(10,2))
     );
 
-    println!("{}", BinaryTree::collapse(
-        &Box::new(bt.head.expect("No head initialized"))
-    ))
+    println!("{}", bt.value());
 }
